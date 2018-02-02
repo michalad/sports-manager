5e643a516caf59431a17dd0d64ab7e8735cfdcea
Lukasz Potepa <lukasz.potepa@ocado.com>
2018-02-02 17:12:32 +0100

    Sport event details page

--- frontend/src/components/pages/sportEventDetailsPage.js
+++ frontend/src/components/pages/sportEventDetailsPage.js
@@ -1,13 +1,43 @@
-"use strict"
-import React from 'react';
+"use strict";
+import React from "react";
+import {connect} from "react-redux";
+import {Col, Table} from "react-bootstrap";
 
 class SportEventDetailsPage extends React.Component {
     render() {
-        return (<div>
-            <h1>Sports Event Details Page</h1>
-        </div>)
+
+        const {
+            matches
+        } = this.props;
+
+        return (
+            <div>
+                <Col xs={8} md={4}>
+                    <MatchesTable matches={matches}/>
+                </Col>
+            </div>
+        );
     }
 }
+
+const MatchesTable = ({matches}) => (
+    <Table striped bordered condensed hover>
+        <tbody>
+        <MatchRows matches={matches}/>
+        </tbody>
+    </Table>
+);
+
+const MatchRows = ({matches}) => (
+    matches.map(match => (
+        <tr>
+            <td>{match.teamA.name}</td>
+            <td>{`${match.teamA.result} - ${match.teamB.result}`}</td>
+            <td>{match.teamB.name}</td>
+        </tr>
+    ))
+
+);
 
 const mapStateToProps = state => {
     console.log('mapStateToProps', state);
@@ -17,7 +47,6 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {};
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(SportEventDetailsPage);
 
