40db195f094b312fa7f52a9f7ce718a33644498c
michal.adamek <michal.adamek@ocado.com>
2018-02-19 20:13:06 +0100

    Configure heroku deploy

--- webpack.prd.config.js
+++ webpack.prd.config.js
@@ -30,5 +30,12 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 }
